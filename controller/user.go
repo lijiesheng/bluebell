7b4610f2da32155abdcee10bd4a83e77af131bea
“lijiesheng” <ljs_hsm@163.com>
2022-08-22 23:05:42 +0800

    request.go

--- controller/user.go
+++ controller/user.go
@@ -2,14 +2,27 @@ package controller
 
 import (
 	"bluebell/logic"
-	"github.com/gin-gonic/gin"
+	"bluebell/models"
 	"net/http"
+
+	"github.com/go-playground/validator/v10"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
-func SignUpHandler(c *gin.Context)  {
+func SignUpHandler(c *gin.Context) {
 	// 1. 获取参数和参数校验
-	c.Query("")
-
+	p := &models.ParamSignUp{} // 是不是等价于 new(models.ParamSignUp)
+	if err := c.ShouldBindJSON(p); err != nil {
+		// 请求参数有误，直接返回响应
+		zap.L().Error("SignUp with invalid params ", zap.Error(err))
+		// 判断 err 是不是 validator.ValidationErrors 类型
+		errs, ok := err.(validator.ValidationErrors)
+		if !ok {
+			Respi
+		}
+	}
 
 	// 2. 业务处理
 	logic.SignUp()
